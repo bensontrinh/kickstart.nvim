c508d9583c5de636660b562bdcd7c93f118ef75d
Benson Trinh <trinh.benson@gmail.com>
2024-10-30 16:24:19 -0600

    adjusting some settings

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,10 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        typescriptreact = { 'biomejs' },
+        typescript = { 'biomejs' },
+        javascript = { 'biomejs' },
+        javascriptreact = { 'biomejs' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
