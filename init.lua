cc9519e3e2d20235148d4841aa0eb306c0d8742c
Benson Trinh <trinh.benson@gmail.com>
2024-11-02 13:22:49 -0600

    adding in go settings and mason deps

--- init.lua
+++ init.lua
@@ -655,6 +655,7 @@ require('lazy').setup({
         'rust-analyzer',
         'typescript-language-server',
         'fixjson',
+        'gomodifytags',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -902,7 +903,31 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'go',
+        'gomod',
+        'gosum',
+        'javascript',
+        'json',
+        'yaml',
+        'sql',
+        'csv',
+        'gitignore',
+        'proto',
+        'typescript',
+        'rust',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
